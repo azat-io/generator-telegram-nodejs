31edb21d60ea8ce7a6399f116991bc3fe074ebc1
Azat S <to@azat.io>
2016-03-24 23:41:22 +0300

    Init

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,22 +7,43 @@ module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
 
-    // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ultimate ' + chalk.red('TelegramNodejs') + ' generator!'
+      'Hi! I\'m a generator of your future Node.js ' + chalk.green('Telegram Bot') + '!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        name: 'githubname',
+        message: 'What is your GitHub username?'
+      },
+      {
+        name: 'botname',
+        message: 'What is the bot name?'
+      },
+      {
+        name: 'botusername',
+        message: 'What is the bot username?'
+      },
+      {
+        name: 'botdescription',
+        message: 'What does this bot will do?'
+      },
+      {
+        name: 'token',
+        message: 'What is the bot secret token?'
+      }
+    ];
 
     this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
+      this.name = props.name;
+      this.githubname = props.githubname;
+      this.botname = props.botname;
+      this.botdescription = props.botdescription;
+      this.token = props.token;
       done();
     }.bind(this));
   },
@@ -30,16 +51,29 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
     app: function () {
       this.fs.copy(
+        this.templatePath('_app.js'),
+        this.destinationPath('app.js')
+      );
+      this.fs.copy(
+        this.templatePath('_CHANGELOG.md'),
+        this.destinationPath('CHANGELOG.md')
+      );
+      this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
       );
       this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
+        this.templatePath('_LICENSE.md'),
+        this.destinationPath('LICENSE.md')
       );
-    },
-
-    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('_README.md'),
+        this.destinationPath('README.md')
+      );
+      this.fs.copy(
+        this.templatePath('_token.js'),
+        this.destinationPath('token.js')
+      );
       this.fs.copy(
         this.templatePath('editorconfig'),
         this.destinationPath('.editorconfig')
@@ -48,10 +82,17 @@ module.exports = yeoman.generators.Base.extend({
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
       );
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+    },
+
+    projectfiles: function () {
     }
   },
 
-  install: function () {
-    this.installDependencies();
-  }
+  // install: function () {
+  //   this.installDependencies();
+  // }
 });
---
+++ generators/app/templates/_README.md
@@ -0,0 +1,13 @@
+# <%= botname %>
+
+<a href="https://telegram.org"><img align="right" width="130" height="130" src="https://telegram.org/img/t_logo.png"></a>
+
+<%= botdescription %>
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+MIT © [<%= name %>](https://github.com/<%= githubname %>)
